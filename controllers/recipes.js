1483c849738351b7785bbfa2fa9ecc4d7fad8c68
Michelle Pitts <michelle.m.pitts@gmail.com>
2019-12-30 17:17:15 -0500

    added create functionality and new recipe form

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,4 +1,5 @@
 const Recipe = require('../models/recipe')
+const User = require('../models/user')
 
 const index = (req, res) => {
     Recipe.find({}, function(err, recipes){
@@ -19,7 +20,46 @@ const show = (req, res) => {
             })
         })
 }
+const newRecipe = (req, res) => {
+    res.render('recipes/new', {
+        title: 'New Recipe',
+        user: req.user
+    })
+}
+const create = (req, res) => {
+    let recipe = new Recipe({
+        name: req.body.name,
+        photo: req.body.photo,
+        ingredients: req.body.ingredients.split('; '),
+        directions: req.body.directions.split('; '),
+        notes: req.body.notes,
+        tags: req.body.tags.split('; '),
+    })
+    if (req.user) {
+        User.findById(req.user._id, function(err, user){
+            user.recipes.push(recipe._id);
+            user.save(function(err) {
+                console.log(user);
+            })
+        })
+    }
+    recipe.save(function(err) {
+        if (err) return res.redirect('/recipes/new');
+        console.log(recipe);
+        res.redirect('/recipes')
+    })
+    //need to create new recipe
+    //then add recipe to user object
+
+}
+const deleteRecipe = (req, res) => {
+    //for delete, need to find all forks and fork parents and remove from those lists or handle gaps somehow
+}
+
 module.exports = {
     index,
-    show
+    show,
+    new: newRecipe,
+    create,
+    delete: deleteRecipe
 }
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const recipeCtrl = require('../controllers/recipes.js');
 
 router.get('/', recipeCtrl.index);
+router.post('/', recipeCtrl.create);
+router.get('/new', recipeCtrl.new);
 router.get('/:id', recipeCtrl.show);
 
 module.exports = router;
