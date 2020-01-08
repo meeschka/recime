c892d7723fd652dded12aa80f9421f1bab99203a
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-08 13:13:44 -0500

    added styling for search bar

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -269,20 +269,6 @@ const update = async function(req, res) {
         })
 }
 
-// let storage = multer.diskStorage({
-//     filename: function(req, file, callback) {
-//       callback(null, Date.now() + file.originalname);
-//     }
-//   });
-// let imageFilter = function (req, file, cb) {
-//     // accept image files only
-//     if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-//         return cb(new Error('Only image files are allowed!'), false);
-//     }
-//     cb(null, true);
-// };
-// let upload = multer({ storage: storage, fileFilter: imageFilter})
-
 module.exports = {
     index,
     show,
